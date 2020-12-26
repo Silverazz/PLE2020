1f224eb37a0df7aec117f9a011bd9359a7a27549
alegendre001 <alexandre.legendre@etu.u-bordeaux.fr>
2020-12-26 14:31:29 +0100

    Extract hashtags

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -10,6 +10,8 @@ import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.util.StatCounter;
+import org.json.*;
+import scala.Tuple2;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -36,23 +38,59 @@ public class Project {
     }
 
     public static void topK(JavaRDD<String> data){
-        TreeMap<Integer, String> top10 = new TreeMap<>();
+        
 
         //JavaRDD<String> output = context.parallelize(result);
 
-        //ArrayList<String> top = output.top(10);
+       // ArrayList<String> top = output.top(10);
+    }
+
+    public static Iterator<String> extractHashtagsFromLine(String line){
+        ArrayList<String> result = new ArrayList();
+
+        JSONObject json = new JSONObject(line);
+        JSONObject entities = json.getJSONObject("entities");
+
+        if(entities == null){
+            return result.iterator();
+        }
+
+        JSONArray hashtags = entities.getJSONArray("hashtags");
+
+        if(hashtags == null){
+            return result.iterator();
+        }
+
+        for(int i = 0; i < hashtags.length(); i++){
+            result.add(hashtags.getJSONObject(i).getString("text"));
+        }
+
+        return result.iterator();
     }
 
     public static void main(String[] args) {
 
 	    SparkConf conf = new SparkConf().setAppName("Projet PLE 2020");
-	    JavaSparkContext context = new JavaSparkContext(conf);
+	    context = new JavaSparkContext(conf);
         fillRessources();
 
         //Day 01 for start
-        JavaRDD<String> lines = context.textFile(RESSOURCES_URLS[0]);
-        System.out.println(lines.take(2));
-        
+        JavaRDD<String> lines = context.textFile(RESSOURCES_URLS[4]);
+/*
+        JavaPairRDD<String, Integer> = lines
+            .flatMap(line -> Arrays.asList(line.split(",")).iterator())
+            .mapToPair(hashtag -> new Tuple2<>(hashtag, 1))
+            .reduceByKey((a, b) -> a + b)
+            .top(10);*/
+
+        //JavaRDD<String> test = lines.flatMap(line -> Arrays.asList(line.split(",")).iterator());
+        //JavaRDD<String> tryt = test.filter(line -> line.contains("hashtags"));
+
+        JavaRDD<String> test = lines.flatMap(line -> extractHashtagsFromLine(line));
+
+        System.out.println(test.take(20));
+
+
 	    context.close();
 	}
 }
