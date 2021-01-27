0b513b0344bd3d52821d7a8863db3c4e294021e7
alegendre001 <alexandre.legendre@etu.u-bordeaux.fr>
2021-01-27 01:08:53 +0100

    fix question d)

---
+++ src/main/java/sparkjobs/UserListForHashtag.java
@@ -0,0 +1,44 @@
+package bigdata;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.json.*;
+import scala.Tuple2;
+
+import java.util.*;
+
+public class UserListForHashtag extends SparkJob{
+
+    public static Iterator<Tuple2<String, String>> extractTupleHashtagUserFromLine(String line){
+        List<Tuple2<String, String>> result = new ArrayList();
+        JSONObject json = null;
+        try {
+            json = new JSONObject(line);
+        }catch(Exception e){ }
+
+        if(json != null){
+            JSONArray hashtags = retrieveHashtags(json);
+            String user = retrieveUser(json);
+            if(hashtags != null && user != null){
+                for(int i = 0; i < hashtags.length(); i++){
+                    String hashtag = hashtags.getJSONObject(i).getString("text");
+                    Tuple2<String, String> tuple = new Tuple2<String, String>(hashtag, user);
+                    result.add(tuple);
+                }
+            }
+        }
+
+        return result.iterator();
+    }
+
+    public static void runJob(){
+        JavaPairRDD<String, Iterable<String>> test = GlobalManager.data
+            .flatMapToPair(line -> extractTupleHashtagUserFromLine(line))
+            .distinct()
+            .groupByKey();
+
+        System.out.println(test.take(10));
+    }
+}
+
