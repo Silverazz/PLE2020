cce22b3f5065559c911766ba01de12cb32d19877
Jean-David AKATA <akata.jda@gmail.com>
2021-01-28 21:57:35 +0100

    first version final

---
+++ littleFront/scripts/user-hashtags.js
@@ -0,0 +1,48 @@
+const table = document.querySelector('body > div > table > tbody');
+const batchsize = 15;
+
+let next = null;
+let prev = null;
+
+let generateTable = async (start) => {
+    try {
+        let response = await fetch(`http://localhost:9090/user-hashtags/${start}/${batchsize}`);
+        if (response.ok) {
+            let data = await response.json();
+            for(lang of data){
+                let str = lang.$.replace('\n', '')
+                table.appendChild(createRow(lang.key, str));
+            }
+            prev = start
+            next = data.pop().key
+        }
+        else{
+            alert("error catched");
+        }
+    } catch (error) {
+        alert('Catch :'+error);
+    }
+}
+
+let createRow = (username, nbTweet) => {
+    let elt = document.createElement('tr');
+    elt.innerHTML = `<th scope="row">${username}</th>\
+                        <td>${nbTweet}</td>`;
+    return elt;
+}
+
+let nextBtn = document.querySelector('body > div > button:nth-child(2)')
+
+nextBtn.addEventListener('click', (event) => {
+    table.innerHTML = ''
+    generateTable(next)
+})
+
+let prevBtn = document.querySelector('body > div > button:nth-child(1)')
+
+prevBtn.addEventListener('click', (event) => {
+    table.innerHTML = ''
+    generateTable(prev)
+})
+
+generateTable('*')
